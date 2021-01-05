683936b5d54b3a933e5e55d0136bc7502f79273a
yaonn <>
2021-01-05 22:22:04 +0800

    this code can work

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import cv2 as cv
 import sys
 import numpy as np
+import pyzbar.pyzbar as pyzbar
+
+def decode(im) : 
+    # Find barcodes and QR codes
+    decodedObjects = pyzbar.decode(im)
+    # Print results
+    # for obj in decodedObjects:
+    #     print(f'Type : {obj.type}'')
+    #     print(f'Data : {obj.data,}\n')     
+    return decodedObjects
+
+font = cv.FONT_HERSHEY_SIMPLEX
 
 # Display barcode and QRcode location
 def display(im, bbox):
@@ -14,42 +26,54 @@ def display(im, bbox):
 def main():
   # initialize the cam
   webcam = cv.VideoCapture(0, cv.CAP_DSHOW)
-  # initialize the cv2 QRCode detector
-  # detector = cv.QRCodeDetector()
-
-  # webcamS = (int(webcam.get(cv.CAP_PROP_FRAME_WIDTH)), int(webcam.get(cv.CAP_PROP_FRAME_HEIGHT)))
   WIN_WEBCAM = 'webcam'
   cv.namedWindow(WIN_WEBCAM, cv.WINDOW_AUTOSIZE)
 
-  framenum = -1 # Frame counter
-  while True: # Show the image captured in the window and repeat
+  while (webcam.isOpened()): # Show the image captured in the window and repeat
     _, frameWebcam = webcam.read()
 
     if frameWebcam is None:
       print('<<<Game Over>>>')
       break
+    im = cv.cvtColor(frameWebcam, cv.COLOR_BGR2GRAY)
+    decodedObjects = decode(im)
     
-    # if qrcodeDetector.detectAndDecodeMulti(frameWebcam):
-    # data, bbox, _ = detector.detectAndDecode(frameWebcam)
-    # # check if there is a QRCode in the image
-    # if bbox is not None:
-    #   # display the image with lines
-    #   display(frameWebcam, bbox)
-    #   if data:
-    #     print(f'[+] QRCode detected, data:{data}')
+    for decodedObject in decodedObjects: 
+      points = decodedObject.polygon
     
-      # rectifiedImage = np.uint8(rectifiedImage)
-      # cv.imshow('Results', rectifiedImage)
-      # break
+      # If the points do not form a quad, find convex hull
+      if len(points) > 4 : 
+        hull = cv.convexHull(np.array([point for point in points], dtype=np.float32))
+        hull = list(map(tuple, np.squeeze(hull)))
+      else : 
+        hull = points
+      
+      # Number of points in the convex hull
+      n = len(hull)     
+      # Draw the convext hull
+      for j in range(0,n):
+        cv.line(frameWebcam, hull[j], hull[ (j+1) % n], (255,0,0), 3)
 
-    framenum += 1
+      x = decodedObject.rect.left
+      y = decodedObject.rect.top
 
+      print(x, y)
+      print(f'Type : {decodedObject.type}')
+      print(f'Data : {decodedObject.data,}\n')
+
+      barCode = str(decodedObject.data)
+      cv.putText(frameWebcam, barCode, (x, y), font, 1, (0,255,255), 2, cv.LINE_AA)
+
+    # Display the resulting frame
     cv.imshow(WIN_WEBCAM, frameWebcam)
 
-    k = cv.waitKey(30)
-    if k == 27:
+    key = cv.waitKey(3)
+    if key & 0xFF == ord('q'):
       break
+    elif key & 0xFF == ord('s'): # wait for 's' key to save 
+        cv.imwrite('Capture.png', frameWebcam)
 
+  # When everything done, release the capture
   webcam.release()
   cv.destroyAllWindows()
   sys.exit(0)
